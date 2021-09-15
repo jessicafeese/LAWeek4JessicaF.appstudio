4d029d8bb7e2b4bebebcc2d52a3182c5c89bd405
jessicafeese <jaf32246@creighton.edu>
2021-09-15 16:23:07 -0500

    LAWeek4

--- forms/pets/pets.js
+++ forms/pets/pets.js
@@ -1,9 +1,13 @@
 
 let petNames = ['Darcy','Jack','Jerry','Marty','Vito','Vinny','Riley','Saddie','Marge','Sam']
 
-let withSal = [petNames + ',' + 'Sal']
+let withSal = petNames
+withSal.push('Sal')
 console.log(withSal)
 
 console.log(`The third pet is named ${petNames[2]}.`)
 
+console.log(`The array has ${withSal.length} names in it.`);
 
+withSal.pop()
+console.log(withSal)
