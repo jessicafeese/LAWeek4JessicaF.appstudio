22ac821418d9d08eb02603a517faca631edb4ca6
jessicafeese <jaf32246@creighton.edu>
2021-09-15 16:12:09 -0500

    Initial commit

---
+++ forms/pets/pets.js
@@ -0,0 +1,9 @@
+
+let petNames = ['Darcy','Jack','Jerry','Marty','Vito','Vinny','Riley','Saddie','Marge','Sam']
+
+let withSal = [petNames + ',' + 'Sal']
+console.log(withSal)
+
+console.log(`The third pet is named ${petNames[2]}.`)
+
+
